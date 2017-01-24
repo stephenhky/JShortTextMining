b89e434724b3fbcb89c0cd976e05c245c0ab2e59
stephenhky <stephenhky@yahoo.com.hk>
2017-01-24 11:01:17 -0500

    start sumvec classifier

--- src/com/shorttext/sumvec/SumVecClassifier.java
+++ src/com/shorttext/sumvec/SumVecClassifier.java
@@ -1,7 +1,21 @@
 package com.shorttext.sumvec;
 
+import com.shorttext.word2vec.Word2VecUtil;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by hok on 1/19/17.
  */
 public class SumVecClassifier {
+    private Word2VecUtil model;
+
+    public SumVecClassifier(Word2VecUtil model) {
+        this.model = model;
+    }
+
+    public void train(Map<String, List<String>> trainData) {
+
+    }
 }
