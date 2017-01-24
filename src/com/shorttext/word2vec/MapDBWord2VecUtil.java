e31f6a76a2f1b0ab2a0e0cc308b5a13848c5efb6
stephenhky <stephenhky@yahoo.com.hk>
2017-01-24 10:18:50 -0500

    used native BLAS backend successfully

--- src/com/shorttext/word2vec/MapDBWord2VecUtil.java
+++ src/com/shorttext/word2vec/MapDBWord2VecUtil.java
@@ -4,6 +4,7 @@ import org.mapdb.DB;
 import org.mapdb.DBMaker;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.ops.transforms.Transforms;
 
 import java.io.File;
 import java.util.Map;
@@ -19,32 +20,15 @@ public class MapDBWord2VecUtil implements Word2VecUtil {
         this.word2VecMap = db.getHashMap("map");
     }
 
-    @Override
     public double[] getWordVector(String word) {
         return word2VecMap.get(word);
     }
 
-    @Override
     public INDArray getWordMatrix(String word) {
         return Nd4j.create(getWordVector(word));
     }
 
-    @Override
     public double similarity(String word1, String word2) {
-        // native ND4J implementation need a native backend. Use primitive method here.
-        double[] vec1 = getWordVector(word1);
-        double[] vec2 = getWordVector(word2);
-
-        double cosineSum = 0;
-        double vec1SqNorm = 0;
-        double vec2SqNorm = 0;
-
-        for (int i=0; i<vec1.length; i++) {
-            cosineSum += vec1[i]*vec2[i];
-            vec1SqNorm += vec1[i]*vec1[i];
-            vec2SqNorm += vec2[i]*vec2[i];
-        }
-
-        return cosineSum/Math.sqrt(vec1SqNorm*vec2SqNorm);
+        return Transforms.cosineSim(getWordMatrix(word1), getWordMatrix(word2));
     }
 }
