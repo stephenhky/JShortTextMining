70d7ed510ffad0fbd87e8d8ce0f0b64c70c98c70
stephenhky <stephenhky@yahoo.com.hk>
2017-01-24 17:02:00 -0500

    start sumvec classifier

--- src/com/shorttext/sumvec/SumVecClassifier.java
+++ src/com/shorttext/sumvec/SumVecClassifier.java
@@ -1,21 +1,28 @@
 package com.shorttext.sumvec;
 
 import com.shorttext.word2vec.Word2VecUtil;
+import org.nd4j.linalg.api.ndarray.INDArray;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by hok on 1/19/17.
  */
 public class SumVecClassifier {
     private Word2VecUtil model;
+    private Map<String, INDArray> averageVectors;
+
 
     public SumVecClassifier(Word2VecUtil model) {
         this.model = model;
     }
 
     public void train(Map<String, List<String>> trainData) {
+        averageVectors = new ConcurrentHashMap<String, INDArray>();
+        for (String label: trainData.keySet()) {
 
+        }
     }
 }
