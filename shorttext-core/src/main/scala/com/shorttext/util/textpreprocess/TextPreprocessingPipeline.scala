3efaa675ed665a1959efe90b59d14cc868b452a6
stephenhky <stephenhky@yahoo.com.hk>
2018-02-14 15:39:18 -0500

    added preprocessing pipeline class

---
+++ shorttext-core/src/main/scala/com/shorttext/util/textpreprocess/TextPreprocessingPipeline.scala
@@ -0,0 +1,18 @@
+package com.shorttext.util.textpreprocess
+
+import scala.annotation.tailrec
+
+class TextPreprocessingPipeline {
+  val funclist = List( (x: String) => x)
+
+  @tailrec
+  final def preprocessRecursively(text: String, funclist: List[String => String]) : String = {
+    if (funclist.length==0) {
+      text
+    } else {
+      preprocessRecursively(funclist(0)(text), funclist.slice(1, funclist.length))
+    }
+  }
+
+  def preprocess(text: String) : String = preprocessRecursively(text, funclist)
+}
